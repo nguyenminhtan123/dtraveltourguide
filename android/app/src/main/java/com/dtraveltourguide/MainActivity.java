989c4d7385a7f7c31860d02c935a59307655cd8c
Nguyen Minh Tan <tan.nguyen@team.enouvo.com>
2020-07-11 22:45:35 +0700

    base

---
+++ android/app/src/main/java/com/dtraveltourguide/MainActivity.java
@@ -0,0 +1,8 @@
+package com.dtraveltourguide;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+  
+}
---
+++ ios/Pods/Target Support Files/Pods-DTravelTourGuide-tvOSTests/Pods-DTravelTourGuide-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DTravelTourGuide_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_DTravelTourGuide_tvOSTests
+@end
